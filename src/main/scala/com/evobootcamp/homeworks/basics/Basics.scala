a73ac139bf90e6e0a863f9a84cf66a761e1bfe32
Vladislav Lipunov <vladislav.lipunov@egamings.com>
2021-01-27 13:05:54 +0300

    Homework: Basics - refactor gcd

--- src/main/scala/com/evobootcamp/homeworks/basics/Basics.scala
+++ src/main/scala/com/evobootcamp/homeworks/basics/Basics.scala
@@ -3,17 +3,10 @@ package com.evobootcamp.homeworks.basics
 object Basics {
   def lcm(a: Int, b: Int): Int = if (b == 0) a else if (a == 0) b else a * b / gcd(a, b)
 
-  def gcd(a: Int, b: Int): Int = {
-    if (a == 0) {
-      b
-    } else if (b == 0) {
-      a
-    } else if (a > b) {
-      gcd(a - b, b)
-    } else if (a < b) {
-      gcd(a, b - a)
-    } else {
-      a
-    }
+  def gcd(a: Int, b: Int): Int = (a, b) match {
+    case (0, b) => b
+    case (a, 0) => a
+    case _ if (a > b) => gcd(a - b, b)
+    case _ => gcd(a, b - a)
   }
 }
